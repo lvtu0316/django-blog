39e194d19fb772a069ada8cf581574bd284fa31c
lvtu0316 <550407948@qq.com>
2020-04-15 16:53:42 +0800

    删除虚拟环境

--- PIPENV_VENV_IN_PROJECT/BlogProject-tQpI0iF5/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\Python\BlogProject\PIPENV_VENV_IN_PROJECT\BlogProject-tQpI0iF5\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
