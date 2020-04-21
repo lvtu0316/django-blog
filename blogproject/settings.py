82c02852df9691db5bcb5534905274c9faec9afe
lvtu0316 <550407948@qq.com>
2020-04-21 09:17:13 +0800

    settingg

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-b%xlnst+ctq=j!c0(r$)n35*$di*-tmp(oqo#y#&d(petpa43'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'zc0317.com']
 
 
 # Application definition
