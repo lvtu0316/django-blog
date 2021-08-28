740bc6712fbb2a64564c85d498b31a7fea01a75d
lvtu0316 <550407948@qq.com>
2021-08-28 18:46:38 +0800

    评论增加审核

--- comments/admin.py
+++ comments/admin.py
@@ -5,8 +5,15 @@ from comments.models import Comment
 
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ['name', 'email', 'url', 'post', 'created_at']
-    fields = ['name', 'email', 'url', 'text', 'post']
+    list_display = ['name', 'email', 'url', 'post', 'created_at', 'status']
+    fields = ['name', 'email', 'url', 'text', 'post', 'status']
+    '''自定义actions'''
+    actions = ['make_published']
+
+    def make_published(self, request, queryset):
+        queryset.update(status=1)
+
+    make_published.short_description = "审核通过"
 
 
 admin.site.register(Comment, CommentAdmin)
--- comments/models.py
+++ comments/models.py
@@ -5,11 +5,16 @@ from django.utils import timezone
 
 
 class Comment(models.Model):
+    STATUS_CHOICE = [
+        (0, '不通过'),
+        (1, '通过')
+    ]
     name = models.CharField('名字', max_length=50)
     email = models.EmailField('邮箱')
     url = models.URLField('网址', blank=True)
     text = models.TextField('内容')
     created_at = models.DateTimeField('创建时间', default=timezone.now())
+    status = models.IntegerField('审核状态', default=0, choices=STATUS_CHOICE)
     post = models.ForeignKey('blog.Post', verbose_name='文章', on_delete=models.CASCADE)
 
     class Meta:
